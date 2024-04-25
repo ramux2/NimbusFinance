b16060326027c6bfd27cf2f13d4e82f7e1174060
GustavoJannuzzi <gujannuzzi17@gmail.com>
2024-04-25 20:45:46 -0300

    Transaction add

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,3 +1,17 @@
+import { trace } from "next/dist/trace"
+import { Transaction } from "@/components/Transaction";
+
 export default function TransactionPage () {
-    <></>
+    return (
+        <main>
+            <div className="w-80 ml-6 mt-14">
+                <Transaction
+                  amount = "0,00"
+                  date= "24/04/2010"
+                  sourceAccount= "Itau"
+                  targetAccount= "Investimento"
+                />
+            </div>
+        </main>
+    )
 }
